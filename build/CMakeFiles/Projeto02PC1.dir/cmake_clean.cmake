ec14bc5c4b14ef37c3543cf88ab3977205cf76f0
tecma <tecmathe@gmail.com>
2023-11-20 21:51:19 -0300

    Versão final

---
+++ build/CMakeFiles/Projeto02PC1.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto02PC1.dir/src/aritmeticas.c.o"
+  "CMakeFiles/Projeto02PC1.dir/src/aritmeticas.c.o.d"
+  "CMakeFiles/Projeto02PC1.dir/src/control.c.o"
+  "CMakeFiles/Projeto02PC1.dir/src/control.c.o.d"
+  "CMakeFiles/Projeto02PC1.dir/src/leitura.c.o"
+  "CMakeFiles/Projeto02PC1.dir/src/leitura.c.o.d"
+  "CMakeFiles/Projeto02PC1.dir/src/logica.c.o"
+  "CMakeFiles/Projeto02PC1.dir/src/logica.c.o.d"
+  "CMakeFiles/Projeto02PC1.dir/src/main.c.o"
+  "CMakeFiles/Projeto02PC1.dir/src/main.c.o.d"
+  "CMakeFiles/Projeto02PC1.dir/src/memoria.c.o"
+  "CMakeFiles/Projeto02PC1.dir/src/memoria.c.o.d"
+  "CMakeFiles/Projeto02PC1.dir/src/saida.c.o"
+  "CMakeFiles/Projeto02PC1.dir/src/saida.c.o.d"
+  "Projeto02PC1.exe"
+  "Projeto02PC1.pdb"
+  "libProjeto02PC1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projeto02PC1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
